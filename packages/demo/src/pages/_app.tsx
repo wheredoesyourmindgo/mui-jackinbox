ece57bd0939b3fe23d30f06f7af9c9c26f5a4259
Abe Hendricks <ahendricks@pcwa.net>
2021-05-03 16:04:17 -0700

    Make demo compatible with Material-UI

--- packages/demo/src/pages/_app.tsx
+++ packages/demo/src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import React, {useEffect} from 'react'
+import Head from 'next/head'
 import {AppProps} from 'next/app'
 import {ThemeProvider} from '@material-ui/core'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -5,11 +7,26 @@ import theme from '../lib/material-theme'
 import '../styles.css'
 
 export default function MyApp({Component, pageProps}: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles)
+    }
+  }, [])
   return (
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
